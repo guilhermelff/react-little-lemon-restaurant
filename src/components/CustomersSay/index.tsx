181b7c24e28c65684591130ea26783120b3121ad
Guilherme Fernandes <guilhermelff@id.uff.br>
2024-02-01 13:15:14 -0300

    layout updates

--- src/components/CustomersSay/index.tsx
+++ src/components/CustomersSay/index.tsx
@@ -23,7 +23,7 @@ const CustomersSay: FC<HTMLAttributes<HTMLDivElement>> = (props): JSX.Element =>
           rating={3}
           customerImg="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
           customerName="Bruno"
-          testimonial="I have been to many restaurants, but this one certainly stood out. The quality of the ingredients and the preparation of the dishes were impeccable. Not to mention the presentation, which was beautiful. It was an amazing dining experience!"
+          testimonial="I have been to many restaurants, but this one certainly stood out. The quality of the ingredients and the preparation of the dishes were impeccable."
         />
         <CustomersSayCard
           rating={5}
--- src/components/MobileNav/index.tsx
+++ src/components/MobileNav/index.tsx
@@ -3,14 +3,13 @@ import { HashLink } from "react-router-hash-link";
 import { Container, Menu } from "./styles";
 import logoImg from "../../assets/Logo.svg";
 import menuImg from "../../assets/icon _hamburger_menu.svg";
-import basketImg from "../../assets/basket.svg";
 import closeImg from "../../assets/close.svg";
 
 const MobileNav: FC<HTMLAttributes<HTMLElement>> = (props): JSX.Element => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const handleMenu = () => {
-    setMenuOpen(prevValue => !prevValue);
+    setMenuOpen((prevValue) => !prevValue);
   };
 
   return (
@@ -21,7 +20,6 @@ const MobileNav: FC<HTMLAttributes<HTMLElement>> = (props): JSX.Element => {
             <img src={menuImg} alt="Open menu" />
           </button>
         </li>
-        <li> <img className="basketIcon" src={basketImg} alt="Basket" /> </li>
       </ul>
 
       <Menu className={menuOpen ? "opened" : ""}>
@@ -29,12 +27,32 @@ const MobileNav: FC<HTMLAttributes<HTMLElement>> = (props): JSX.Element => {
           <img src={closeImg} alt="Close menu" />
         </button>
         <ul>
-          <li> <img src={logoImg} alt="Little Lemon Logo" /> </li>
-          <li> <HashLink to="/">Home</HashLink> </li>
-          <li> <HashLink to="#about">About</HashLink> </li>
-          <li> <HashLink to="#menu">Menu</HashLink> </li>
-          <li> <HashLink to="/bookings">Reservations</HashLink> </li>
-          <li className="orderBtn"> <HashLink role="button" to="#">Order Online</HashLink> </li>
+          <li>
+            {" "}
+            <img src={logoImg} alt="Little Lemon Logo" />{" "}
+          </li>
+          <li>
+            {" "}
+            <HashLink to="/">Home</HashLink>{" "}
+          </li>
+          <li>
+            {" "}
+            <HashLink to="#about">About</HashLink>{" "}
+          </li>
+          <li>
+            {" "}
+            <HashLink to="#menu">Menu</HashLink>{" "}
+          </li>
+          <li>
+            {" "}
+            <HashLink to="/bookings">Reservations</HashLink>{" "}
+          </li>
+          <li className="orderBtn">
+            {" "}
+            <HashLink role="button" to="#">
+              Order Online
+            </HashLink>{" "}
+          </li>
         </ul>
       </Menu>
     </Container>
